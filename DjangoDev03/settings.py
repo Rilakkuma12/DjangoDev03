ccbf42ae3b54e28a33dcd392053f9b564d767cf7
yangzhiqiong <yangzhiqiong@genomics.cn>
2020-04-13 14:10:08 +0800

    合并

--- DjangoDev03/settings.py
+++ DjangoDev03/settings.py
@@ -37,9 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    
     'projects.apps.ProjectsConfig',
     'interfaces.apps.InterfacesConfig',
     'corsheaders',
+
 ]
 
 MIDDLEWARE = [
@@ -127,4 +130,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+    )
+}
 
