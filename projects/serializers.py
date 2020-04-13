91a8ff9d1ef349be19f4aaea699cf5d1f43a974c
yangzhiqiong <yangzhiqiong@SZ13410_06>
2020-04-13 13:58:21 +0800

    APIView模式简化

--- projects/serializers.py
+++ projects/serializers.py
@@ -47,18 +47,18 @@ class ProjectModelSerializer(serializers.ModelSerializer):
     # 默认情况，父表创建序列化器类不会创建子表字段，需要显示创建
     # 子表字段名为：子表类名小写_set
     # 如果字段定义外键字段，指定related_name='interface',那么这个需要使用related_name
-    interfaces = serializers.StringRelatedField(many=True)
+    # interfaces = serializers.StringRelatedField(many=True)
 
     class Meta:
         model = Projects
         # 指定序列化输出
         # fields = '__all__'
-        fields = ('id' 'name', 'leader', 'tester', 'interfaces_set')
-        exclude = ('create_time', 'update_time')
-        read_only_fields = ('desc', )
+        # fields = ('id' 'name', 'leader', 'tester', 'interfaces_set')
+        exclude = ('id', 'create_time', 'update_time')
+        # read_only_fields = ('desc', )
 
-        extra_kwargs = {
-            'project_name': {
-                'read_only': True
-            }
-        }
+        # extra_kwargs = {
+        #     'project_name': {
+        #         'read_only': True
+        #     }
+        # }
--- projects/views.py
+++ projects/views.py
@@ -2,12 +2,13 @@ import json
 from datetime import timedelta
 from django.http import HttpResponse, JsonResponse
 from django.views import View
+from rest_framework import status
 from rest_framework.generics import GenericAPIView
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from .models import Projects
 from . import serializers
-from rest_framework.views import APIView
 
 # Create your views here.
 
@@ -57,17 +58,7 @@ class ProjectList(APIView):
         """获取项目列表"""
         project_qs = Projects.objects.all()
         serializer = serializers.ProjectSerializer(instance=project_qs, many=True)
-        # project_list = []
-        # for project in project_qs:
-        #     project_list.append({
-        #         'id': project.id,
-        #         'project_name': project.project_name,
-        #         'leader': project.leader,
-        #         'tester': project.tester,
-        #         'create_time': project.create_time.strftime("%Y-%m-%d %H:%M:%S"),
-        #         'update_time': project.update_time.strftime("%Y-%m-%d %H:%M:%S")
-        #                    })
-        return JsonResponse(serializer.data, safe=False)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
     def post(self, request):
         """  
@@ -78,29 +69,13 @@ class ProjectList(APIView):
         3.数据库新增项目
         4.返回单个json（处理结果）
         """
-        python_data = json.loads(request.body.decode('utf-8'))
-        # 方法1
-        # one_project = Projects(project_name=python_data['project_name'],
-        #                        leader=python_data['leader'],
-        #                        tester=python_data['tester'])
-        # one_project.save()
-
-        serializer = serializers.ProjectSerializer(data=python_data)
-        serializer.is_valid(raise_exception=True)
-        # 方法2
-        project = Projects.objects.create(**python_data)
-
-        # one_dict = {
-        #     'id': project.id,
-        #     'project_name': project.project_name,
-        #     'leader': project.leader,
-        #     'tester': project.tester,
-        #     'create_time': (project.create_time + timedelta(hours=8)).strftime("%Y-%m-%d %H:%M:%S"),
-        #     'update_time': (project.update_time + timedelta(hours=8)).strftime("%Y-%m-%d %H:%M:%S")
-        #
-        # }
-        # serializer = serializers.ProjectSerializer(instance=project, many=True)
-        return JsonResponse(serializer.data, safe=False, status=201)
+        serializer = serializers.ProjectSerializer(data=request.data)
+        try:
+            serializer.is_valid(raise_exception=True)
+        except Exception:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
 class ProjectDetail(APIView):
@@ -109,66 +84,25 @@ class ProjectDetail(APIView):
         """获取指定项目信息"""
         # 校验pk
         project = Projects.objects.get(pk=pk)
-        # one_dict = {
-        #     'id': project.id,
-        #     'project_name': project.project_name,
-        #     'leader': project.leader,
-        #     'tester': project.tester,
-        #     'create_time': (project.create_time + timedelta(hours=8)).strftime("%Y-%m-%d %H:%M:%S"),
-        #     'update_time': (project.update_time + timedelta(hours=8)).strftime("%Y-%m-%d %H:%M:%S")
-        #
-        # }
-        serializer = serializers.ProjectSerializer(instance=project)
-        return JsonResponse(serializer.data)
+        serializer = serializers.ProjectModelSerializer(instance=project)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
     def put(self, request, pk):
         """更新指定项目"""
-        # 旧的
         one_project = Projects.objects.get(pk=pk)
 
-        # 新的
-        # python_data = json.loads(request.body.decode('utf-8'))
-        # project = Projects.objects.create(**python_data)
         # 不管是json还是form表单，统一使用request.data
-        serializer = serializers.ProjectSerializer(instance=one_project, data=request.data)
+        serializer = serializers.ProjectModelSerializer(instance=one_project, data=request.data)
         try:
-            serializer.is_valid(raise_exception=True)
+            serializer.is_valid()
         except Exception:
-            return JsonResponse(serializer.errors, status=400)
-        # python_data = serializer.validated_data
-
-        # one_project.id = python_data['id']
-        # one_project.project_name = python_data['project_name']
-        # one_project.leader = python_data['leader']
-        # one_project.tester = python_data['tester']
-        # one_project.create_time = python_data['create_time']
-        # one_project.update_time = python_data['update_time']
-        #
-        # one_project.save()
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         # 调用serializer的create、update方法
         # 同时给instance和data传参,调用save才会调用update
         serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-        # one_dict = {
-        #     'id': one_project.id,
-        #     'project_name': one_project.project_name,
-        #     'leader': one_project.leader,
-        #     'tester': one_project.tester,
-        #     'create_time': (one_project.create_time + timedelta(hours=8)).strftime("%Y-%m-%d %H:%M:%S"),
-        #     'update_time': (one_project.update_time + timedelta(hours=8)).strftime("%Y-%m-%d %H:%M:%S")
-        #
-        # }
-        # serializer = serializers.ProjectSerializer(instance=one_project)
-        return Response(serializer.data, status=201)
-
-    def delete(self, request):
-        pass
-
-
-class ProjectEdit(View):
-
-    def get(self, request):
-        pass
-
-    def post(self, request):
-        pass
+    def delete(self, request, pk):
+        one_project = Projects.objects.get(pk=pk)
+        one_project.delete()
+        return Response(r'{"id":%d}' % pk, status=status.HTTP_204_NO_CONTENT)
