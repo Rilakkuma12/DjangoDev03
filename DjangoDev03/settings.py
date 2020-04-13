9b3ca2cfc57f508aa20d91593594a685aa98f3e8
yangzhiqiong <yangzhiqiong@genomics.cn>
2020-04-13 14:07:29 +0800

    本地配置

--- DjangoDev03/settings.py
+++ DjangoDev03/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'dev03',
         'USER': 'root',
-        'PASSWORD': '',
+        'PASSWORD': '12345678',
         'HOST': 'localhost',
         'PORT': 3306
     }
