3d6ac02265ef89c0902ce8fc36137eaa18ccfca3
yangzhiqiong <yangzhiqiong@SZ13410_06>
2020-04-25 17:03:55 +0800

    使用router优化

--- projects/urls.py
+++ projects/urls.py
@@ -1,20 +1,28 @@
 # 子路由
 from django.urls import path, include
 from . import views
+from rest_framework import routers
 
+router = routers.SimpleRouter()
+# 注册路由
+router.register(r"", views.ProjectViewSet)
 urlpatterns = [
-    path('', views.ProjectViewSet.as_view({
-            "get": "list",
-            "post": "create"
-        })),
-    path('<int:pk>', views.ProjectViewSet.as_view({
-        "get": "retrieve",
-        "put": "update",
-        "delete": "destroy"
-    })),
-    path('names', views.ProjectViewSet.as_view(
-        {
-            "get": "names",
-        }
-    ))
+    path('', include(router.urls))
+    # path('', views.ProjectViewSet.as_view({
+    #         "get": "list",
+    #         "post": "create"
+    #     })),
+    # path('<int:pk>', views.ProjectViewSet.as_view({
+    #     "get": "retrieve",
+    #     "put": "update",
+    #     "delete": "destroy"
+    # })),
+    # path('names', views.ProjectViewSet.as_view(
+    #     {
+    #         "get": "names",
+    #     }
+    # ))
 ]
+
+# 另一种方法
+# urlpatterns += router.urls
